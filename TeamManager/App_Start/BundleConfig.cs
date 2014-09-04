21243d8ac902c9cb8f220e75002e2c35a58e4313
Adrien Folie <folie.adrien@gmail.com>
2014-09-04 02:47:28 +0200

    update dependencies to avoid repo to be tag as a javascript project

--- TeamManager/App_Start/BundleConfig.cs
+++ TeamManager/App_Start/BundleConfig.cs
@@ -11,15 +11,15 @@ namespace TeamManager
             // Utilisez la version de développement de Modernizr pour développer et apprendre. Puis, lorsque vous êtes
             // prêt pour la production, utilisez l'outil de génération sur http://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Dependencies/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/js").Include(
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/jquery-migrate-{version}.js",
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/jquery.validate.js",
-                "~/scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+                "~/Dependencies/Scripts/jquery-{version}.js",
+                "~/Dependencies/Scripts/jquery-migrate-{version}.js",
+                "~/Dependencies/Scripts/bootstrap.js",
+                "~/Dependencies/Scripts/jquery.validate.js",
+                "~/Dependencies/Scripts/jquery.validate.unobtrusive.js",
+                "~/Dependencies/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
                 ));
 
             bundles.Add(new StyleBundle("~/content/css").Include(
